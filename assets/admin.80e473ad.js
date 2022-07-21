e9653190fa99742090f0f183276c224742ba2133
godgodwinter <kakadz.pro@gmail.com>
2022-07-21 13:31:01 +0700

    fix bug persentase chart

--- assets/admin.80e473ad.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./index.56ca7468.js";const i=t({id:"storeAdmin",state:()=>({pagesActive:"dashboard"}),getters:{getPagesActive:e=>e.pagesActive},actions:{setPagesActive(e){this.pagesActive=e}}});export{i as u};
