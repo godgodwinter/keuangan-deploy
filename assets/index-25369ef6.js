1387bfd9024bb61c0eda2bbf1598a8a2c71f970c
godgodwinter <kakadz.pro@gmail.com>
2023-11-06 08:00:57 +0700

    update

--- assets/index-25369ef6.js
+++ assets/index-25369ef6.js
@@ -1,4 +1,4 @@
-import{d as Zr,r as Jr,K as Qr,A as jn,x as ta,B as ea,v as $n}from"./index-a3ee2ccf.js";/*!
+import{d as Zr,r as Jr,K as Qr,A as jn,x as ta,B as ea,v as $n}from"./index-58fc319a.js";/*!
  * Chart.js v3.9.1
  * https://www.chartjs.org
  * (c) 2022 Chart.js Contributors
